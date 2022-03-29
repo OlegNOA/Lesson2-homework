5064c3f627df401bd21f351245c5a85d7afeb87c
OlegNOA <102174193+OlegNOA@users.noreply.github.com>
2022-03-29 20:21:27 +0300

    Add files via upload

---
+++ untitled4/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        checkSumSing();
+    }
+
+    public static void checkSumSing() {
+        int a = 10;
+        int b = 50;
+
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
+
+
